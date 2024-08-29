6c18870592711668d437193e40692a0bc2d2e3f6
Guilherme <guilherme.o.chagas321@gmail.com>
2024-08-29 11:59:25 -0300

    tde3web2

---
+++ tde3web2/script.js
@@ -0,0 +1,73 @@
+//1
+
+/*
+var num = +prompt("Digite um valor");
+var operador = prompt("Digite um operador (+,-,*,/)");
+var num1 = +prompt("Digite outro valor");
+
+switch (operador) {
+  case "+":
+    resultado = num + num1;
+    document.write(`resultado ${resultado}`);
+    break;
+  case "-":
+    resultado = num - num1;
+    document.write(`resultado ${resultado}`);
+    break;
+  case "*":
+    resultado = num * num1;
+    document.write(`resultado é ${resultado}`);
+    break;
+  case "/":
+    resultado = num / num1;
+    document.write(`resultado ${resultado}`);
+  default:
+    document.write("Opções invalidas");
+}*/
+
+//2
+/*
+var valor = +(prompt("Digite o valor da compra"));
+var desconto = prompt("(1)funcionário (2)vip ou (3)");
+
+switch (desconto) {
+  case "1":
+    des = valor * 0.1;
+    valorFinal = valor - des;
+    document.write(`O valor final é ${valorFinal}`);
+    break;
+  case "2":
+    des = valor * 0.05;
+    valorFinal = valor - des;
+    document.write(`O valor final é ${valorFinal}`);
+    break;
+  case "3":
+    document.write(`O valor final é ${valor}`);
+    break;
+
+  default:
+    document.write("Opções invalidas");
+}*/
+
+//3
+
+/*
+
+var valor = +prompt("Digite o valor da compra");
+var forma = prompt("Digite a forma de pagamento 1 (À vista) 2 (À prazo)");
+
+switch (forma) {
+  case "1":
+    des = valor * 0.1;
+    valorFinal = valor - des;
+    document.write(`O desconto é de ${des} e o valor final é ${valorFinal}`);
+    break;
+
+  case "3":
+    document.write(`O valor final é ${valor}`);
+    break;
+  default:
+    document.write("Opções invalidas");
+}*/
+
+//4
