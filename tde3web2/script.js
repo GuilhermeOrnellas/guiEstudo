b34569c9e3c4dcaac5766873ab4ac8c1b804110a
Guilherme <guilherme.o.chagas321@gmail.com>
2024-08-31 01:24:16 -0300

    Update script.js

--- tde3web2/script.js
+++ tde3web2/script.js
@@ -1,73 +1,198 @@
 //1
 
 /*
-var num = +prompt("Digite um valor");
-var operador = prompt("Digite um operador (+,-,*,/)");
-var num1 = +prompt("Digite outro valor");
+var num = +prompt("Digite um valor")
+var operador = prompt("Digite um operador (+,-,*,/)")
+var num1 = +prompt("Digite outro valor")
 
 switch (operador) {
   case "+":
-    resultado = num + num1;
-    document.write(`resultado ${resultado}`);
-    break;
-  case "-":
-    resultado = num - num1;
-    document.write(`resultado ${resultado}`);
-    break;
+    resultado = num + num1
+    document.write(`resultado ${resultado}`)
+    break
+  case "-"
+    resultado = num - num1
+    document.write(`resultado ${resultado}`)
+    break
   case "*":
-    resultado = num * num1;
-    document.write(`resultado é ${resultado}`);
+    resultado = num * num1
+    document.write(`resultado é ${resultado}`)
     break;
   case "/":
-    resultado = num / num1;
-    document.write(`resultado ${resultado}`);
+    resultado = num / num1
+    document.write(`resultado ${resultado}`)
   default:
-    document.write("Opções invalidas");
+    document.write("Opções invalidas")
 }*/
 
 //2
 /*
 var valor = +(prompt("Digite o valor da compra"));
-var desconto = prompt("(1)funcionário (2)vip ou (3)");
+var desconto = prompt("(1)funcionário (2)vip ou (3)")
 
 switch (desconto) {
   case "1":
-    des = valor * 0.1;
-    valorFinal = valor - des;
-    document.write(`O valor final é ${valorFinal}`);
-    break;
+    des = valor * 0.1
+    valorFinal = valor - des
+    document.write(`O valor final é ${valorFinal}`)
+    break
   case "2":
-    des = valor * 0.05;
-    valorFinal = valor - des;
-    document.write(`O valor final é ${valorFinal}`);
+    des = valor * 0.05
+    valorFinal = valor - des
+    document.write(`O valor final é ${valorFinal}`)
     break;
   case "3":
-    document.write(`O valor final é ${valor}`);
+    document.write(`O valor final é ${valor}`)
     break;
 
   default:
-    document.write("Opções invalidas");
+    document.write("Opções invalidas")
 }*/
 
 //3
 
-/*
 
+/*
 var valor = +prompt("Digite o valor da compra");
 var forma = prompt("Digite a forma de pagamento 1 (À vista) 2 (À prazo)");
 
 switch (forma) {
   case "1":
-    des = valor * 0.1;
-    valorFinal = valor - des;
-    document.write(`O desconto é de ${des} e o valor final é ${valorFinal}`);
+    des = valor * 0.1
+    valorFinal = valor - des
+    document.write(`O desconto é de ${des} e o valor final é ${valorFinal}`)
     break;
 
-  case "3":
-    document.write(`O valor final é ${valor}`);
-    break;
+  case "2":
+    document.write(`O valor final é ${valor}`)
+    break
   default:
-    document.write("Opções invalidas");
+    document.write("Opções invalidas")
 }*/
 
 //4
+
+/*
+var conceito = prompt("Digite o seu conceito na matéria Desenvolvimento Web2 (A,B,C,D,E,F)")
+conceito = conceito.toLowerCase();
+
+ switch(conceito){
+
+  case 'a':
+     document.write(`Excelente`)
+    break
+  case 'b':
+     document.write(`Ótimo`)
+    break
+  case 'c':
+     document.write(`Bom`)
+    break
+  case 'd':
+      document.write(`Regular`)
+    break
+  case 'e':
+     document.write(`Ruim`)
+    break
+  case 'f':
+     document.write(`Nos vemos de novo ano que vem`)
+    break
+  default:
+    document.write(`Erro`)
+ }*/
+
+
+// 5 
+/*
+var letra = prompt("Digite uma letra")
+
+tem como simplificar com if massssss
+switch(letra){
+  case 'a':
+  case 'e':
+  case 'i':
+  case 'o':
+  case 'u':
+    document.write(`Vogal`)
+  break
+  case 'b':
+  case 'c':
+  case 'd':
+  case 'f':
+  case 'g':
+  case 'h':
+  case 'j':
+  case 'k':
+  case 'l':
+  case 'm':
+  case 'n':
+  case 'p':
+  case 'q':
+  case 'r':
+  case 's':
+  case 't':
+  case 'v':
+  case 'w':
+  case 'x':
+  case 'y':
+  case 'z':
+    document.write(`Consoante`)
+      break
+  default:
+    document.write("Erro")
+}
+*/
+
+
+// 6
+
+
+// for (i = 1; i <= 9; i++) {
+//   document.write(`<br>${i}`)
+//   if (i % 2 == 0) {
+//    console.log(`<br>${i}`);
+//   }
+//  }
+
+// 7
+
+
+// for (nota = -1; nota > 10 || nota < 0; ) {
+ 
+//   var nota= +(prompt("Digite uma nota"))
+
+//   if (nota <= 10 && nota >= 0){
+    
+//     document.write(`A nota digitada foi ${nota}`)
+//   }
+//   else {
+//     alert (`Nota inválida`);
+//  }
+// }
+
+// 8 dsadasd
+
+// for (var i = 0; i<2; i++) {
+//     var usuario= prompt("Digite o usuário")
+//     var senha= prompt("Digite a senha")
+  
+//     if (usuario !== senha){
+//       document.write(`concluido`)
+//     break
+//     }
+//     else {
+//       i--
+//       alert (`erro`);
+//    }
+  
+// }
+
+// 9
+
+
+
+
+
+
+
+
+
