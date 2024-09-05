e735397319cdf5bab94cac1d1cea0ba13f2f983d
Guilherme <guilherme.o.chagas321@gmail.com>
2024-09-05 12:49:03 -0300

    tde4web2

---
+++ tde4web2/script.js
@@ -0,0 +1,56 @@
+// // exercicio 1 
+// var a = 1
+// while (a<=10){
+//   document.write(`${a}`)
+//a++
+// }
+//exercicio 2 
+//var n = Number(prompt('digite um numero '))
+// var a=1
+// while(a<=n){
+//   a++
+//   var res=a+n
+//   document.write(`o resultaddo é ${res}`)
+// }
+//exercicio3
+// var a=20
+// do{
+//   document.write(`<br>${a}`)
+//   a--
+// }while(a >= 10)
+//   document.write(`lançamento`)
+//exercicio4
+// var opção 
+// do{
+//   console.log ('menu de operações : 1loop-2responde e sai-3sair')
+//   //opção= +(prompt('escolha uma opçao')) 
+//     switch(opção){
+//       case 1 :
+//        //opção= +(prompt('escolha uma opçao'))
+//         break
+//           case 2:
+//         document.write('saindo')
+//         break
+//       case 3 :
+//         break
+//     
+//     }
+//   }
+// exercicio 5
+// c = Math.floor(Math.random()* 11)
+// var acerto = false
+// var senhaacertada = 0
+// do{
+//   // j= + (prompt(`qual sua senha`))
+//   if(j == c){
+//     acerto = true
+//   }
+// }while (acerto == false)
+// document.write(`palpites ${senhaacertada}`)
+// document.write(`senha foi ${c}`)
+//exercicio 6
+// var nome = +(prompt('digite um nome )
+// function saudacao(nome){
+//   document.write(`oi!${nome}`)
+// }
+//ercicio 7 
