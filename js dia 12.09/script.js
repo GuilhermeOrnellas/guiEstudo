cb1835245b723cb09c92ee55eec72cbf3f36d25b
Guilherme <guilherme.o.chagas321@gmail.com>
2024-09-12 11:47:06 -0300

    Update script.js

--- js dia 12.09/script.js
+++ js dia 12.09/script.js
@@ -103,7 +103,80 @@ document.write(`<br>O numero 12 encontra-se na Array? ${f.includes(12)}`)
 */
 
 //push()- adiciona um novo elemento no final do Array -------------------------
-
+/*
 var h = [10,20,30,40]
 h.push(50)
 document.write(`<br>${h}`)
+
+//pop- remove o utimo elemento da Array --------------
+
+h.pop()
+document.write(`<br>${h}`)
+
+//unshift - adiciona um novo elemento no inicio da Array -----------
+
+h.unshift(0)
+document.write(`<br>${h}`)
+
+//shift() - remove o primeiro elemento na Array
+
+h.shift()
+document.write(`<br>${h}`)
+*/
+//slice()- fatia a Array sem alterar o Array original ------
+/*
+aluno='Maria Da Silva Xavier'
+a=aluno.slice(5)
+document.write(`<br>${a}`)
+a=aluno.slice(5,14)
+document.write(`<br>${a}`)
+
+email = 'exemplo@gmail.com'
+e=email.slice(email.indexOf('a')+1)
+document.write(`<br>${e}`)
+*/
+
+/*splice() - 1º valor: posição 
+             2º valor: quantidade de elementos excluidos
+             3º valor: novos elementos */
+/*
+d=['Sedunda','Terça','Quarta']
+d1=d.splice(1,1)
+document.write(`<br> A ${d1} foi deletada`)
+document.write(`<br> Sobram - ${d} `)
+
+d2= d.splice(1,1,'Quinta','Sexta')
+document.write(`<br> A ${d2} foi deletada`)
+document.write(`<br>${d}`)
+
+d3= d.splice(1,0,'Quinta','Sexta')
+document.write(`<br> A ${d3} foi deletada`)
+document.write(`<br>${d}`)*/
+/*
+nome = []
+for(i=0;i<5;i++){
+    nome[i]=prompt('Digite o seu nome')
+}
+document.write(`<br>Nome: ${nome}`)
+n=nome.splice(2,0,1)
+document.write(`<br>Nome: ${nome}`)*/
+
+//Função anonima - não possui nome, atribui-se a função a uma variável
+/*
+var msg=function(){
+    return 'Olá,bom dia'
+}
+document.write(`<br>${msg()}`)
+// ---------------------------------
+var msg1=function(){
+    return `Olá ${nome}, bom dia`
+}
+nome=prompt('Digite o seu nome')
+document.write(`<br>${msg1()}`)
+// ---------------------------------
+var numero=function(num){
+    return num**2
+}
+num=+(prompt('Digite um numero'))
+document.write(`<br>${numero(num)}`)
+*/
