49dc1dc3075f93e3765f2b12ef83ba680c64bdde
Guilherme <guilherme.o.chagas321@gmail.com>
2024-09-17 19:30:17 -0300

    cruso.js

---
+++ curso.js/script.js
@@ -0,0 +1,19 @@
+// A4
+/*
+ window.alert('My first mensage')
+// or
+ alert('lest learn js')
+
+ confirm('are you enjoying js')
+
+ prompt('What's  your name')
+  name=prompt("what's your name")
+ document.write(`O seu nome é ${name}`)
+
+*/
+// B5
+
+
+
+
+
