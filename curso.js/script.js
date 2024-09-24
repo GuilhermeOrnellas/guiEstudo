1f6616260e99571fe7a21f19092016995bc163df
Guilherme <guilherme.o.chagas321@gmail.com>
2024-09-24 19:13:21 -0300

    Update script.js

--- curso.js/script.js
+++ curso.js/script.js
@@ -11,8 +11,18 @@
  document.write(`O seu nome é ${name}`)
 
 */
-// B5
+// B
+// (modo antigo) -----------------------------------------------------------------------------------
 
+// document.write(`A soma dos valores` + variavel) serve para concatenar grudar um ao outro
+//  e se vc colocar Number.paerseInt depois do = no var, vai somar pois a string vai virar number
+
+// (number + number) para adição  + 
+// (string + string ) para concatenação   String()
+// mas é melhor para mim usar só o + antes do prompt
+// -------------------------------------------------------------------------------------------------
+
+// .replace('oque','por oque') serve para mudar o oque(ex: '.') pelo o por oque(ex: ',')
 
 
 
