185b8756786d8d7839d054566290b0211d538bfe
Guilherme <guilherme.o.chagas321@gmail.com>
2024-11-07 19:04:37 -0300

    TropaBet.io

---
+++ projeto cassino web/script.js
@@ -0,0 +1,22 @@
+var header = document.getElementById('header');
+var navigation = document.getElementById('navigation');
+var content = document.getElementById('content');
+var showSidebar = false;
+
+function sidebarbotao() {
+  
+  showSidebar =!showSidebar;
+  if (showSidebar) {
+    navigation.style.marginLeft = '-10%';
+    navigation.style.animationName = 'showSidebar';
+    content.style.backdropFilter = 'showDropdown';
+  } else {
+    navigation.style.marginLeft = '-100%';
+    navigation.style.animationName = '';
+  }
+}
+function closeSidebar() {
+  if (showSidebar) {
+    sidebarbotao();
+  }
+}
